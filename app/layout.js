8ff4a3d53def6d9dc97904a237d70803ec2bfd5f
Mostafa Meerzad <mostafameerzad@gmail.com>
2025-10-06 18:37:37 +0430

    change favicon

--- app/layout.js
+++ app/layout.js
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "AfghanPet",
   description: "AfghanPet - All your pet needs in one place",
+  icons: {
+    icon: "/favicon.png",
+  },
 };
 
 export default function RootLayout({ children }) {
