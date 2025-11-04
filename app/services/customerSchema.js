aaad48decc0cef01eac84bd4f546cff687502b52
Mostafa Meerzad <mostafameerzad@gmail.com>
2025-11-04 12:56:43 +0430

    fix the validation error, that was breaking the UI

--- app/services/customerSchema.js
+++ app/services/customerSchema.js
@@ -3,21 +3,17 @@ import { z } from "zod";
 const baseCustomerSchema = z.object({
   name: z
     .string("Name is required")
-    .min(2, "Name should at least be 2 characters long")
+    .min(1, "Name should at least be 2 characters long")
     .max(100, "Name too long"),
   phone: z
-  .string()
-  .regex(
-    /^(\d{10}|\+?[0-9 ]{10,15})$/,
-    "Phone number must be 10 digits or a valid international format"
-  )
-  .optional(),
-  email: z.string().email("Please enter a valid email").optional(),
-  address: z
     .string()
-    .min(10, "Invalid address")
-    .max(255, "Address too long")
+    .regex(
+      /^(\d{10}|\+?[0-9 ]{10,15})$/,
+      "Phone number must be 10 digits or a valid international format"
+    )
     .optional(),
+  email: z.string().optional(),
+  address: z.string().max(255, "Address too long").optional(),
 });
 
 //  For creation, name is required
